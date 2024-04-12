dda2db9a575bbbe531e1595b3e76766c247a0a5b
Luis Henrique Zimmermann <lhzimmermann@gmail.com>
2024-04-12 18:13:31 -0300

    atualiza teste

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Login with Google/i);
+  const linkElement = screen.getByText(/Fazer Login com o Google/i);
   expect(linkElement).toBeInTheDocument();
 });
